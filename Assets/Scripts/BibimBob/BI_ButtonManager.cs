fa367b32a11950ce4457dcc6f1cdee0f0e09a161
1403wlaud <jeemyung1129@gmail.com>
2024-02-25 23:36:11 +0900

    .

--- Assets/Scripts/BibimBob/BI_ButtonManager.cs
+++ Assets/Scripts/BibimBob/BI_ButtonManager.cs
@@ -6,33 +6,21 @@ using UnityEngine.UI;
 
 public class BI_ButtonManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> list = new List<GameObject>();
+    [SerializeField] List<GameObject> GomyungList = new List<GameObject>();
     [SerializeField]List<GameObject> Sauce = new List<GameObject>();
     [SerializeField] List<GameObject> _Rice = new List<GameObject>();
+    List<GameObject> ChangeList;
     string BtnDuplicationClickCheck;
     string ObjName;
     string ObjTag;
-    List<GameObject> ChangeList;
+    bool SauceMake = false;
     int _count = 0;
     int Count=0;
     public static int Point = 0;
     bool BtnClickcheck = false;
     bool check=true;
 
-
-
     public GameObject NextBtn;
-    public GameObject GoSari;
-    public GameObject Beef;
-    public GameObject Pork;
-    public GameObject Sugar;
-    public GameObject Spinach;
-    public GameObject Carrot;
-    public GameObject Boochu;
-    public GameObject Kimchi;
-    public GameObject GreenOnionChecks;
-    public GameObject LittlePumpkin;
-    public Image rice;
     public GameObject MakeRice;
     public GameObject Gomyung;
     public GameObject MakeSauce;
@@ -63,6 +51,8 @@ public class BI_ButtonManager : MonoBehaviour
             MakeRice.SetActive(false);
             Gomyung.SetActive(true);
             BtnClickcheck = false;
+            ChangeList = GomyungList;
+            SauceMake = true;
             Count++;
         }
         else if (Count == 1)
@@ -71,6 +61,7 @@ public class BI_ButtonManager : MonoBehaviour
             MakeSauce.SetActive(true);
             BtnClickcheck = false;
             ChangeList = Sauce;
+            SauceMake=false;
             Count++;
         }
     }
@@ -91,10 +82,16 @@ public class BI_ButtonManager : MonoBehaviour
     {
         BtnClickcheck = true;
         GameObject clickBtnObject = EventSystem.current.currentSelectedGameObject;
-        if (check == true){
-            if (_count >= 1){
-                if (clickBtnObject.tag != ObjTag){
-                    GameObject.Find(ObjName).SetActive(false);
+        if (SauceMake != true)
+        {
+            if (check == true)
+            {
+                if (_count >= 1)
+                {
+                    if (clickBtnObject.tag != ObjTag)
+                    {
+                        GameObject.Find(ObjName).SetActive(false);
+                    }
                 }
             }
         }
@@ -116,71 +113,10 @@ public class BI_ButtonManager : MonoBehaviour
                 if (go.tag == clickBtnObject.tag){
                     go.SetActive(false);
                     check = false;
-                    BtnDuplicationClickCheck = "NULL";
+                    BtnDuplicationClickCheck = "?";
                 }
             }
         }
         
-    }
-    public void Btn()
-    {
-        BtnClickcheck = true;
-        GameObject clickBtnObject = EventSystem.current.currentSelectedGameObject;
-        foreach (GameObject go in list)
-        {
-            if (go == clickBtnObject)
-            {
-                if (clickBtnObject.name == "고사리")
-                {
-                    GoSari.SetActive(true);
-                    Point++;
-                }
-                if (clickBtnObject.name == "돼지고기")
-                {
-                    Pork.SetActive(true);
-                    Point--;
-                }
-                if (clickBtnObject.name == "소고기")
-                {
-                    Beef.SetActive(true);
-                    Point++;
-                }
-                if (clickBtnObject.name == "각설탕")
-                {
-                    Sugar.SetActive(true);
-                    Point--;
-                }
-                if (clickBtnObject.name == "시금치")
-                {
-                    Spinach.SetActive(true);
-                    Point++;
-                }
-                if (clickBtnObject.name == "당근")
-                {
-                    Carrot.SetActive(true);
-                    Point++;
-                }
-                if (clickBtnObject.name == "부추")
-                {
-                    Boochu.SetActive(true);
-                    Point--;
-                }
-                if (clickBtnObject.name == "김치")
-                {
-                    Kimchi.SetActive(true);
-                    Point--;
-                }
-                if (clickBtnObject.name == "파맛첵스")
-                {
-                    GreenOnionChecks.SetActive(true);
-                    Point--;
-                }
-                if (clickBtnObject.name == "애호박")
-                {
-                    LittlePumpkin.SetActive(true);
-                    Point++;
-                }
-            }
-        }
     }
 }
