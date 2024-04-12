febfaa215f3142d82928c98526e0ba6292414694
24기구원 <koowonqwe2@gmail.com>
2024-04-12 21:50:32 +0900

    Minji

---
+++ Assets/Scripts/MainMenue/DevleoperMin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevleoperMin : MonoBehaviour
+{
+   
+    private float speed = 3.0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 1 * speed * Time.deltaTime, 0);
+       
+    }
+}
